eea0bf2febda059230802debd9c2092d7eb0c78d
Pestov Sergey <pestov-sa@ya.ru>
2026-01-30 11:48:04 +0300

    keymap updated

--- keyboards/sofle/keymaps/psqq/keymap.c
+++ keyboards/sofle/keymaps/psqq/keymap.c
@@ -34,47 +34,47 @@ bool isKbArrows = true;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LAYER_0] = LAYOUT(
 //--- begin of auto align
-  KC_ESC,   KC_1,        KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,        KC_7,           KC_8,           KC_9,           KC_0,      KC_MINS,
-  KC_GRV,   KC_Q,        KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,        KC_U,           KC_I,           KC_O,           KC_P,      KC_LBRC,
-  KC_TAB,   KC_A,        KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,        KC_J,           KC_K,           KC_L,           KC_SCLN,   KC_QUOT,
-  KC_BSLS,  KC_Z,        KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,  KC_MSTP,  KC_N,        KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,   KC_RBRC,
-                         KC_LGUI,  KC_LALT,  KC_LCTL,  TL_LOWR,  KC_LSFT,  KC_RSFT,  TL_UPPR,     KC_RCTL,        KC_RALT,        KC_RGUI
+  KC_ESC,   KC_1,        KC_2,     KC_3,     KC_4,     KC_5,                              KC_6,        KC_7,           KC_8,           KC_9,           KC_0,      KC_MINS,
+  KC_GRV,   KC_Q,        KC_W,     KC_E,     KC_R,     KC_T,                              KC_Y,        KC_U,           KC_I,           KC_O,           KC_P,      KC_LBRC,
+  KC_TAB,   KC_A,        KC_S,     KC_D,     KC_F,     KC_G,                              KC_H,        KC_J,           KC_K,           KC_L,           KC_SCLN,   KC_QUOT,
+  KC_BSLS,  KC_Z,        KC_X,     KC_C,     KC_V,     KC_B,          KC_MUTE,  KC_MSTP,  KC_N,        KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,   KC_RBRC,
+                         KC_LGUI,  KC_LALT,  KC_LCTL,  TL_LOWR,       KC_LSFT,  KC_RSFT,  TL_UPPR,     KC_RCTL,        KC_RALT,        KC_RGUI
 //--- end of auto align
 ),
 [_LAYER_1] = LAYOUT(
 //--- begin of auto align
-  _______,  _______,     _______,  _______,  _______,  _______,                      _______,     _______,        _______,        _______,        _______,   _______,
-  _______,  _______,     _______,  _______,  _______,  _______,                      _______,     _______,        _______,        _______,        _______,   _______,
-  _______,  _______,     _______,  _______,  _______,  _______,                      _______,     _______,        _______,        _______,        _______,   _______,
-  _______,  _______,     _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,        _______,        _______,   _______,
-                         _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,        _______
+  _______,  _______,     _______,  _______,  _______,  _______,                           _______,     _______,        _______,        _______,        _______,   _______,
+  _______,  _______,     _______,  _______,  _______,  _______,                           _______,     _______,        _______,        _______,        _______,   _______,
+  _______,  _______,     _______,  _______,  _______,  _______,                           _______,     _______,        _______,        _______,        _______,   _______,
+  _______,  _______,     _______,  _______,  _______,  _______,       _______,  _______,  _______,     _______,        _______,        _______,        _______,   _______,
+                         _______,  _______,  _______,  _______,       _______,  _______,  _______,     _______,        _______,        _______
 //--- end of auto align
 ),
 [_LOWER] = LAYOUT(
 //--- begin of auto align
-  KC_F1,    KC_F2,       KC_F3,    KC_F4,    KC_F5,    KC_F6,                        KC_F7,       KC_F8,          KC_F9,          KC_F10,         KC_F11,    KC_F12,
-  KC_PSCR,  _______,     _______,  _______,  _______,  _______,                      _______,     KC_P7,          KC_P8,          KC_P9,          _______,   KC_SCRL,
-  KC_CAPS,  KC_PSLS,     KC_PAST,  KC_PMNS,  KC_PPLS,  KC_PEQL,                      _______,     KC_P4,          KC_P5,          KC_P6,          KC_P0,     KC_NUM,
-  KC_INS,   _______,     _______,  _______,  _______,  _______,  _______,  _______,  _______,     KC_P1,          KC_P2,          KC_P3,          _______,   KC_PAUSE,
-                         _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,        _______
+  KC_F1,    KC_F2,       KC_F3,    KC_F4,    KC_F5,    KC_F6,                             KC_F7,       KC_F8,          KC_F9,          KC_F10,         KC_F11,    KC_F12,
+  KC_PSCR,  _______,     _______,  _______,  _______,  KC_EQL,                            _______,     KC_P7,          KC_P8,          KC_P9,          _______,   KC_SCRL,
+  KC_CAPS,  KC_PSLS,     KC_PAST,  KC_PMNS,  KC_PPLS,  KC_PEQL,                           KC_ENT,      KC_P4,          KC_P5,          KC_P6,          KC_P0,     KC_NUM,
+  KC_INS,   KC_DEL,      _______,  _______,  _______,  _______,       _______,  _______,  _______,     KC_P1,          KC_P2,          KC_P3,          _______,   KC_PAUSE,
+                         _______,  _______,  _______,  _______,       _______,  _______,  _______,     _______,        _______,        _______
 //--- end of auto align
 ),
 [_UPPER] = LAYOUT(
 //--- begin of auto align
-  _______,  _______,     _______,  _______,  MS_WHLL,  MS_WHLR,                      KC_STOGGLE,  KC_PDF_0,       KC_PDF_L,       KC_PDF_U,       KC_PDF_A,  _______,
-  _______,  C(KC_BSPC),  MS_BTN3,  MS_BTN2,  MS_BTN1,  MS_WHLU,                      KC_PGUP,     KC_PRVWD,       KC_SUP,         KC_NXTWD,       KC_HOME,   _______,
-  _______,  KC_DEL,      KC_BSPC,  KC_ENT,   KC_SPC,   MS_WHLD,                      KC_PGDN,     KC_SLEFT,       KC_SDOWN,       KC_SRIGHT,      KC_END,    OSM(MOD_LGUI),
-  _______,  C(KC_Z),     C(KC_X),  C(KC_C),  C(KC_V),  _______,  _______,  _______,  _______,     OSM(MOD_LSFT),  OSM(MOD_LCTL),  OSM(MOD_LALT),  _______,   _______,
-                         _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,        _______
+  _______,  _______,     MS_BTN5,  MS_BTN4,  MS_WHLL,  MS_WHLR,                           KC_STOGGLE,  KC_PDF_0,       KC_PDF_L,       KC_PDF_U,       KC_PDF_A,  _______,
+  _______,  C(KC_BSPC),  MS_BTN3,  MS_BTN2,  MS_BTN1,  MS_WHLU,                           KC_PGUP,     KC_PRVWD,       KC_SUP,         KC_NXTWD,       KC_HOME,   _______,
+  _______,  KC_DEL,      KC_BSPC,  KC_PEQL,  KC_SPC,   MS_WHLD,                           KC_PGDN,     KC_SLEFT,       KC_SDOWN,       KC_SRIGHT,      KC_END,    OSM(MOD_LGUI),
+  _______,  C(KC_Z),     C(KC_X),  C(KC_C),  C(KC_V),  LGUI(KC_SPC),  _______,  _______,  KC_ENT,      OSM(MOD_LSFT),  OSM(MOD_LCTL),  OSM(MOD_LALT),  _______,   _______,
+                         _______,  _______,  _______,  _______,       _______,  _______,  _______,     _______,        _______,        _______
 //--- end of auto align
 ),
   [_ADJUST] = LAYOUT(
 //--- begin of auto align
-  _______,  _______,     _______,  _______,  _______,  _______,                      _______,     _______,        _______,        _______,        _______,   _______,
-  _______,  _______,     _______,  _______,  _______,  _______,                      _______,     _______,        MS_UP,          _______,        _______,   _______,
-  _______,  _______,     _______,  _______,  _______,  _______,                      _______,     MS_LEFT,        MS_DOWN,        MS_RGHT,        _______,   _______,
-  _______,  _______,     _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,        _______,        _______,   _______,
-                         _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,        _______,        _______
+  _______,  _______,     _______,  _______,  _______,  _______,                           _______,     _______,        _______,        _______,        _______,   _______,
+  _______,  _______,     _______,  _______,  _______,  _______,                           _______,     MS_BTN5,        MS_UP,          MS_BTN4,        _______,   _______,
+  _______,  _______,     _______,  _______,  _______,  _______,                           _______,     MS_LEFT,        MS_DOWN,        MS_RGHT,        _______,   _______,
+  _______,  _______,     _______,  _______,  _______,  _______,       _______,  _______,  _______,     _______,        _______,        _______,        _______,   _______,
+                         _______,  _______,  _______,  _______,       _______,  _______,  _______,     _______,        _______,        _______
 //--- end of auto align
   )
 };
